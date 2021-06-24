6627019f20724f995f80d7e7fdb8225645a739ee
SoniaMichelle <gonzalezortizmichelle@gmail.com>
2021-06-24 09:42:48 -0500

    Actualizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdditemComponent } from './components/additem/additem.component';
+import { ItemsComponent } from './components/items/items.component';
 
-const routes: Routes = [];
+const router: Routes = [
+  /* Espesificacion de las rutas */
+  {
+    path: '', /* Index tradicional */
+    component: ItemsComponent /* propiedad que pide el componente que queremos cargar */
+  },
+  {
+    path: 'add',
+    component: AdditemComponent
+  }
+  
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(router)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
